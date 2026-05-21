4626a73dcd090584558c057a6331566e280bc7a6
Samuel Wong <wongsamuel2006@gmail.com>
2026-05-21 14:22:28 +0800

    connection changes

--- database/connection.py
+++ database/connection.py
@@ -1,12 +1,20 @@
-# POSTGRESQL CONNECTIONS
-import psycopg2
+from psycopg2 import pool
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_pool = pool.SimpleConnectionPool(
+    1, 20,
+    host=os.getenv("POSTGRES_HOST"),
+    database=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    port=os.getenv("POSTGRES_PORT")
+)
 
 def get_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="ecommerce",
-        user="postgres",
-        password="postgres",
-        port=5433
-    )
-    return conn
+    return db_pool.getconn()
+
+def return_connection(conn):
+    db_pool.putconn(conn)
